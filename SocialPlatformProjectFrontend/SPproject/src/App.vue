db788b73651d2730d9ba4007370828ab2cd24ea6
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2022-01-24 15:07:14 +0100

    login with logo (div not working atm) + pages for different views

--- SocialPlatformProjectFrontend/SPproject/src/App.vue
+++ SocialPlatformProjectFrontend/SPproject/src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+  <main>
+      <Navbar/>
+      <div>
+       <Body/>
+      </div>
+    
+  </main>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Body from './components/Body.vue'
+
+export default{
+  name: "App",
+  components: {
+    Navbar,
+    Body,
+  },
+}
+
+</script>
+
 
 <style>
 
