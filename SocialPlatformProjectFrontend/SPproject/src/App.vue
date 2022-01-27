fcb73432ff3045b5b5f0bbd2cdfab39f6291b6d1
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2022-01-27 14:45:57 +0100

    more stuff removed

--- SocialPlatformProjectFrontend/SPproject/src/App.vue
+++ SocialPlatformProjectFrontend/SPproject/src/App.vue
@@ -3,10 +3,6 @@
         <Navbar />
         <div>
             <Category />
-            <!-- <div id="Thread-control">
-          <Thread/>
-        </div> -->
-            <!-- <createPost /> -->
             <router-view />
         </div>
         <Footer />
@@ -18,31 +14,16 @@ import Navbar from './components/Navbar.vue'
 import Category from './components/Category.vue'
 import Footer from './components/Footer.vue'
 import Thread from './components/Thread.vue'
-import createPost from './components/CreatePost.vue'
 
 export default {
     name: 'App',
     components: {
-        createPost,
         Navbar,
         Category,
         Thread,
         Footer,
     },
-    // data() {
-    //     return {
-    //         isModalVisible: false,
-    //     }
-    // },
-    // methods: {
-    //     showModal() {
-    //         this.isModalVisible = true
-    //     },
-
-    //     closeModal() {
-    //         this.isModalVisible = false
-    //     },
-    // },
+    
 }
 </script>
 
--- SocialPlatformProjectFrontend/SPproject/src/components/CategoryFolderPages/Computers.vue
+++ SocialPlatformProjectFrontend/SPproject/src/components/CategoryFolderPages/Computers.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-      <h1>COMPUTERS CATEGORY</h1>
         <div>
             <Thread/>
         </div>
