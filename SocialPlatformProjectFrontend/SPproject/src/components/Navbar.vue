7b2d597cf6770fbd2ada757c4e547dc264421526
CarlMM <kaptenkeps@gmail.com>
2022-02-01 11:11:02 +0100

    formatfixing

--- SocialPlatformProjectFrontend/SPproject/src/components/Navbar.vue
+++ SocialPlatformProjectFrontend/SPproject/src/components/Navbar.vue
@@ -1,163 +1,158 @@
 <template>
-    <header id="header">
-        <div class="header-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-sm-6 col-6 header-top-left">
-                        <div id="logo">
-                            <router-link to="/">
-                                <img
-                                    src="/src/assets/Group2_logo.jpg"
-                                    alt=""
-                                    title=""
-                                    height="110"
-                                />
-                            </router-link>
-                        </div>
+  <header id="header">
+    <div class="header-top">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 col-sm-6 col-6 header-top-left">
+            <div id="logo">
+              <router-link to="/">
+                <img
+                  src="/src/assets/Group2_logo.jpg"
+                  alt=""
+                  title=""
+                  height="110"
+                />
+              </router-link>
+            </div>
+          </div>
+          <div class="col-lg-6 col-sm-6 col- header-top-right">
+            <div class="nav-menu">
+              <div class="authDiv">
+                <div v-if="!AuthState.loading">
+                  <div v-if="!AuthState.isAuthenticated">
+                    <a href="#" @click.prevent="login()">Log in - Register</a>
+                  </div>
+                  <div v-else>
+                    <div class="logged-in">
+                      <p>
+                        {{ AuthState.user.nickname }}
+                        <!-- authstate.user.sub = user token (id) -->
+                        <select @change="changeRoute($event)">
+                          <option selected value="">Home</option>
+                          <option value="MyProfile">Profile</option>
+                          <option value="MyGroups">My groups</option>
+                          <option value="MyThreads">My threads</option>
+                        </select>
+                        <img :src="AuthState.user.picture" alt="AvatarPic" />
+                        <a href="#" @click.prevent="logout()">Logout</a>
+                      </p>
                     </div>
-                    <div class="col-lg-6 col-sm-6 col- header-top-right">
-                        <div class="nav-menu">
-                            <div class="authDiv">
-                                <div v-if="!AuthState.loading">
-                                    <div v-if="!AuthState.isAuthenticated">
-                                        <a href="#" @click.prevent="login()"
-                                            >Log in - Register</a
-                                        >
-                                    </div>
-                                    <div v-else>
-                                        <div class="logged-in">
-                                            <p>
-                                                {{ AuthState.user.nickname }}
-                                                <!-- authstate.user.sub = user token (id) -->
-                                          <select @change="changeRoute($event)"> 
-                                              <option selected>Home</option>
-                                            <option value="MyProfile">Profile</option>
-                                            <option value="MyGroups">My groups</option>
-                                            <option value="MyThreads">My threads</option> 
-                                          </select>
-                                        <img :src="AuthState.user.picture" alt="AvatarPic">
-                                        <a href="#" @click.prevent="logout()" >Logout</a>
-                                      </p>
-                                  </div>
-                              </div>
-                          </div>
-                          <div v-else>Loading ...</div>
-                      </div>
+                  </div>
                 </div>
+                <div v-else>Loading ...</div>
+              </div>
             </div>
+          </div>
         </div>
-            </div>
-        </div>
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
-import Dropdown from './Dropdown.vue'
+import Dropdown from "./Dropdown.vue";
 
 export default {
-   data() {
-     return {
-       
-     };
+  data() {
+    return {};
   },
-   methods:{
-    changeRoute(e){
-      this.$router.push('/' + e.target.value)
-
-     }
+  methods: {
+    changeRoute(e) {
+      this.$router.push("/" + e.target.value);
     },
-}
+  },
+};
 </script>
 
 <script setup>
-import { useAuth0, AuthState } from '../auth0/useAuth0.js'
+import { useAuth0, AuthState } from "../auth0/useAuth0.js";
 
-const { login, logout, initAuth } = useAuth0(AuthState)
+const { login, logout, initAuth } = useAuth0(AuthState);
 
-initAuth()
+initAuth();
 </script>
 
 <style scoped>
 .logged-in > p > img {
-    height: 6vh;
+  height: 6vh;
 }
 
 #user {
-    color: rgb(255, 153, 0);
-    padding-right: 25px;
+  color: rgb(255, 153, 0);
+  padding-right: 25px;
 }
 
 #nav-menu-container {
-    display: inline-block;
-    padding-top: 10px;
+  display: inline-block;
+  padding-top: 10px;
 }
 
 a {
-    -webkit-transition: all 0.3s ease 0s;
-    -moz-transition: all 0.3s ease 0s;
-    -o-transition: all 0.3s ease 0s;
-    transition: all 0.3s ease 0s;
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+  transition: all 0.3s ease 0s;
 }
 
 p {
-    font-size: 18px;
-    font-weight: bolder;
-    color: #fff;
+  font-size: 18px;
+  font-weight: bolder;
+  color: #fff;
 }
 
 i {
-    font-size: 18px;
-    font-weight: bolder;
-    color: #fff;
+  font-size: 18px;
+  font-weight: bolder;
+  color: #fff;
 }
 
 /*Header*/
 .header-top {
-    font-size: 12px;
-    padding: 6px 0px;
-    background-color: #33393a;
+  font-size: 12px;
+  padding: 6px 0px;
+  background-color: #33393a;
 }
 
 .header-top a {
-    color: rgb(255, 255, 255);
-    -webkit-transition: all 0.3s ease 0s;
-    -moz-transition: all 0.3s ease 0s;
-    -o-transition: all 0.3s ease 0s;
-    transition: all 0.3s ease 0s;
+  color: rgb(255, 255, 255);
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+  transition: all 0.3s ease 0s;
 }
 
 .header-top a:hover {
-    color: #ff9900;
+  color: #ff9900;
 }
 
 .header-top .header-top-left a {
-    margin-right: 8px;
+  margin-right: 8px;
 }
 
 .header-top .header-top-right {
-    text-align: right;
+  text-align: right;
 }
 
 .header-top .header-top-right .header-social a {
-    color: #fff;
-    margin-left: 15px;
-    -webkit-transition: all 0.3s ease 0s;
-    -moz-transition: all 0.3s ease 0s;
-    -o-transition: all 0.3s ease 0s;
-    transition: all 0.3s ease 0s;
+  color: #fff;
+  margin-left: 15px;
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+  transition: all 0.3s ease 0s;
 }
 
 .header-top .header-top-right .header-social a:hover {
-    color: #ff7b00;
+  color: #ff7b00;
 }
 
 .main-menu {
-    padding-top: 10px;
-    background: #33393a;
-    padding-left: 15px;
-    padding-right: 15px;
-    border-bottom: solid 2px;
-    border-color: black;
+  padding-top: 10px;
+  background: #33393a;
+  padding-left: 15px;
+  padding-right: 15px;
+  border-bottom: solid 2px;
+  border-color: black;
 }
 
 /*--------------------------------------------------------------
@@ -166,20 +161,20 @@ i {
 /* Nav Menu Essentials */
 .nav-menu,
 .nav-menu * {
-    margin: 0;
-    padding: 0;
-    list-style: none;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
 /* Nav Meu Styling */
 .nav-menu a {
-    padding: 0 8px 0px 8px;
-    text-decoration: none;
-    display: inline-block;
-    color: #fff;
-    font-weight: 500;
-    font-size: 12px;
-    text-transform: uppercase;
-    outline: none;
+  padding: 0 8px 0px 8px;
+  text-decoration: none;
+  display: inline-block;
+  color: #fff;
+  font-weight: 500;
+  font-size: 12px;
+  text-transform: uppercase;
+  outline: none;
 }
 </style>
