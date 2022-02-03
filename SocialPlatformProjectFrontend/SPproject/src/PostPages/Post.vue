280a058497ed0814bfa4b794495af95cbb18ed13
CarlMM <kaptenkeps@gmail.com>
2022-02-03 19:22:00 +0100

    preparing fetch specificpost

--- SocialPlatformProjectFrontend/SPproject/src/PostPages/Post.vue
+++ SocialPlatformProjectFrontend/SPproject/src/PostPages/Post.vue
@@ -36,6 +36,9 @@ export default {
         //this.fetchRepliesForPost();
         this.fetchReplyForPost();
     },
+    created(){
+        this.$store.dispatch('GetRepliesForSpecificPost')
+    },
 
     computed: {
         getSpecificThread() {
