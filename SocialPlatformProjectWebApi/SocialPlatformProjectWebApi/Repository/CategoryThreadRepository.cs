d488382dc6a0d0ab617c9b035d1fd60269d3dc45
ThomasLohr <73110720+ThomasLohr@users.noreply.github.com>
2022-02-03 14:03:23 +0100

    Post categorythread implemented

--- SocialPlatformProjectWebApi/SocialPlatformProjectWebApi/Repository/CategoryThreadRepository.cs
+++ SocialPlatformProjectWebApi/SocialPlatformProjectWebApi/Repository/CategoryThreadRepository.cs
@@ -42,7 +42,7 @@ namespace SocialPlatformProjectWebApi.Repository
             return result;
         }
 
-        public async Task<CategoryThread> AddCateoryThread(CategoryThread categoryThread)
+        public async Task<CategoryThread> AddCategoryThread(CategoryThread categoryThread)
         {
             DateTime date = DateTime.Now;
 
@@ -51,6 +51,7 @@ namespace SocialPlatformProjectWebApi.Repository
 
             var template = new CategoryThread
             {
+                
                 Title = categoryThread.Title,
                 Text = categoryThread.Text,
                 CreatedDate = date,
@@ -60,7 +61,8 @@ namespace SocialPlatformProjectWebApi.Repository
             };
 
             await _dbContext.AddAsync(template);
-
+            await _dbContext.SaveChangesAsync();
+                    
             return template;
         }
     }
