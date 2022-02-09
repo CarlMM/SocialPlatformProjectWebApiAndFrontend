c580d01c54d2253c9d62f853d4f48d9ef7fc1445
aleksandarradinovic <Alekss.radinovic@hotmail.com>
2022-02-09 10:21:15 +0100

    Man kan ta bort specific thread, man kan också hämta lista över alla users i Admin vyn som Admin

--- SocialPlatformProjectFrontend/SPproject/src/Views/Adminpages/AdminAllUsers.vue
+++ SocialPlatformProjectFrontend/SPproject/src/Views/Adminpages/AdminAllUsers.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
-        <h1 style="color: white">this is all ÚSERS admin page</h1>
+    <div v-for="user in this.$store.state.listOfUsersAdmin" :key="user.id">
+        <h3>{{ user.username }}</h3>
+        <h3>{{ user.email }}</h3>
     </div>
 </template>
 
 <script>
 export default {
     name: 'AdminAllUsers.vue',
+    computed: {
+        getAllUsersAdmin() {
+            return this.$store.dispatch('getAllUsersAdmin')
+        },
+    },
+    created() {
+        this.$store.dispatch('getAllUsersAdmin')
+    },
 }
 </script>
+
+<style scoped>
+h3 {
+    color: #fff;
+}
+</style>
